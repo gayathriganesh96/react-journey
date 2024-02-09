345b9c7522292a25a8fbb94333a7b4da15837858
gayathriganesh96 <gayathrimanju7@gmail.com>
2024-02-09 11:06:49 +0530

    v2 changes

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -11,17 +11,27 @@ function App() {
   };
 
   const addTask = () => {
-    const newTodoList = [...todoList, newTask];
-    setTodoList(newTodoList);
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+    // const newTodoList = [...todoList, newTask];
+    // setTodoList(newTodoList);
     setNewTask(""); // Clear the input field after adding the task
   };
 
-  const deleteTask = (taskName) => {
-    const newTodoList = todoList.filter((task) => {
-      return task !== taskName; // Simplified filter condition
-    });
-    setTodoList(newTodoList);
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
   };
+
+
+  // const deleteTask = (taskName) => {
+  //   const newTodoList = todoList.filter((task) => {
+  //     return task !== taskName; // Simplified filter condition
+  //   });
+  //   setTodoList(newTodoList);
+  // };
 
   return (
     <div className="App">
@@ -30,10 +40,10 @@ function App() {
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="list">
-        {todoList.map((task, index) => ( // Added unique key prop and fixed syntax
-          <div key={index}>
-            <h1>{task}</h1>
-            <button onClick={() => deleteTask(task)}>x</button>
+        {todoList.map((task) => ( // Added unique key prop and fixed syntax
+          <div>
+            <h1>{task.taskName}</h1>
+            <button onClick={() => deleteTask(task.id)}>x</button>
           </div>
         ))}
       </div>
